a2cec67c632c994889d2d3a1eadce84caef41b12
Kevin <kevinholtkamp26@gmail.com>
2022-04-01 17:56:48 +0200

    Reformat code with format defined in .editorconfig

--- functions.ps1
+++ functions.ps1
@@ -2,12 +2,15 @@ Function Test-Symlink($Path){
     ((Get-Item $Path).Attributes.ToString() -match "ReparsePoint")
 }
 #Source: https://stackoverflow.com/a/59048942
-Function Create-Association($ext, $exe) {
+Function Create-Association($ext, $exe){
     $name = cmd /c "assoc $ext 2>NUL"
-    if ($name) { # Association already exists: override it
+    if($name){
+    # Association already exists: override it
         $name = $name.Split('=')[1]
-    } else { # Name doesn't exist: create it
-        $name = "$($ext.Replace('.',''))file" # ".log.1" becomes "log1file"
+    }
+    else{
+    # Name doesn't exist: create it
+        $name = "$($ext.Replace('.', ''))file" # ".log.1" becomes "log1file"
         cmd /c 'assoc $ext=$name'
     }
     cmd /c "ftype $name=`"$exe`" `"%1`""
