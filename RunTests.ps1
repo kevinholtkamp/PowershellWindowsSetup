26d5e55c14507a71e113fc2df1fb47bc100b8d9a
Kevin <kevinholtkamp26@gmail.com>
2022-04-10 12:53:38 +0200

    More verbose RunTests.ps1

--- RunTests.ps1
+++ RunTests.ps1
@@ -1,6 +1,9 @@
-Set-ExecutionPolicy RemoteSigned
+Write-Host "Installing NuGet and Pester" -ForegroundColor Green
 Install-PackageProvider -Name NuGet -Force -Confirm:$false
 Install-Module -Name Pester -MinimumVersion "5.0.0" -Force -SkipPublisherCheck -Confirm:$false
 
-
+Write-Host "Invoking Pester" -ForegroundColor Green
 Invoke-Pester ".\Tests\" -PassThru -Output Detailed
+
+Write-Host "Tests finnished" -ForegroundColor Green
+Read-Host
