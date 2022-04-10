8c2bccb3bbc4aa6702f3710470eb5c7d25527b4e
Kevin <kevinholtkamp26@gmail.com>
2022-04-10 12:55:07 +0200

    Removed error output of Get-InstalledModule and formatted for easier disabling of output

--- Tests/CommonTestParameters.ps1
+++ Tests/CommonTestParameters.ps1
@@ -1,12 +1,9 @@
 Set-Variable -Name "TestGroup" -Value "Tests\testGroup" -Scope Global
-Set-Variable -Name "DebugPreference" -Value "silentlyContinue" -Scope Global
-Set-Variable -Name "ErrorActionPreference" -Value "silentlyContinue" -Scope Global
-Set-Variable -Name "VerbosePreference" -Value "silentlyContinue" -Scope Global
 
-if(!(Get-InstalledModule PsIni)){
+if(!(Get-InstalledModule PsIni -ErrorAction "silentlyContinue")){
     Install-Module -Name PsIni -Force -Confirm:$false
 }
-if(!(Get-InstalledModule Recycle)){
+if(!(Get-InstalledModule Recycle -ErrorAction "silentlyContinue")){
     Install-Module -Name Recycle -Force -Confirm:$false
 }
 
@@ -14,7 +11,13 @@ Mock -CommandName Write-Host -MockWith {}
 Mock -CommandName Write-Debug -MockWith {}
 Mock -CommandName Write-Verbose -MockWith {}
 Mock -CommandName Read-Host -MockWith {}
+Set-Variable -Name "DebugPreference" -Value "silentlyContinue" -Scope Global
+Set-Variable -Name "ErrorActionPreference" -Value "silentlyContinue" -Scope Global
+Set-Variable -Name "VerbosePreference" -Value "silentlyContinue" -Scope Global
 
 #Mock -CommandName Write-Debug -MockWith {Write-Host $Prompt}
 #Mock -CommandName Write-Verbose -MockWith {Write-Host $Prompt}
 #Mock -CommandName Read-Host -MockWith {Write-Host $Prompt}
+#Set-Variable -Name "DebugPreference" -Value "continue" -Scope Global
+#Set-Variable -Name "ErrorActionPreference" -Value "continue" -Scope Global
+#Set-Variable -Name "VerbosePreference" -Value "continue" -Scope Global
