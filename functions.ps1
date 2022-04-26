fd1eb55d78b9ecade4b7ee98e1763f8daf582976
Kevin <kevinholtkamp26@gmail.com>
2022-04-26 16:13:35 +0200

    New functions Format-Path, Resolve-Symlink and Compare-Paths

--- functions.ps1
+++ functions.ps1
@@ -28,3 +28,61 @@ Function Create-Association($Extension, $Executable){
     }
     cmd /c "ftype $Name=`"$Executable`" `"%1`""
 }
+#Which Verb? Format? Optimize? Convert? Resolve? Unify is unapproved but fitting
+Function Format-Path(){
+    [Cmdletbinding()]
+    param($Path)
+
+    Write-Verbose "Format-Path: $Path"
+    if($Path.StartsWith(".")){
+        #Get Absolute path if path starts with "."
+        $Path = Resolve-Path -Path $Path
+        Write-Verbose "Resolved Path: $Path"
+    }
+    if($Path -match "^.*::(.*)"){
+        $Path = $Path -replace "^.*::(.*)", '$1'
+        Write-Verbose "Replaced Powershell providers: $Path"
+    }
+    $Path = $Path -replace "/"                      , "\" `
+                  -replace "^\\\\\.\\"              , "" `
+                  -replace "^\\\\\?\\"              , "" `
+                  -replace "^UNC\\"                 , "\\"
+    Write-Verbose "Replaced UNC conventions: $Path"
+
+    if($Path -match "^\\\\([A-Za-z]+)(@SSL)?"){
+        $Path = $Path -replace "^\\\\([A-Za-z]+)(@SSL)?", "\\$((Resolve-DnsName $matches[1] -Type "A").IPAddress)"
+        Write-Verbose "Resolve name into IP: $Path"
+    }
+
+    return $Path.TrimEnd("\")
+}
+Function Resolve-Symlink(){
+    [Cmdletbinding()]
+    param($Path)
+
+    Write-Verbose "Resolve-Symlink: $Path"
+    $Path = Format-Path $Path
+    Write-Verbose "Formatted: $Path"
+    $Current = $Path
+    while($Current){
+        Write-Verbose "Current step: $Current"
+        if(Test-Symlink $Current){
+            $Target = Get-SymlinkTarget $Current
+            Write-Verbose "Step is Symlink, replacing with target: $Target"
+            $Path = $Path -replace "^$([System.Text.RegularExpressions.Regex]::Escape($Current))", "$Target"
+            $Current = $Target
+        }
+        else{
+            $Current = Split-Path -Path $Current -Parent -ErrorAction Stop
+        }
+    }
+    Write-Verbose "Resolved Symlink: $Path"
+    return Format-Path $Path
+}
+Function Compare-Paths(){
+    [Cmdletbinding()]
+    param([String]$First, [String]$Second)
+
+    Write-Verbose "Comparing-Paths: '$First' to '$Second'"
+    return (Resolve-Symlink $First -ErrorAction Stop) -eq (Resolve-Symlink $Second -ErrorAction Stop)
+}
