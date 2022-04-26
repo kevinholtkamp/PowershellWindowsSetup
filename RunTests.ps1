b8e23b046bc8927ab209a2e769577023aac117b4
Kevin <kevinholtkamp26@gmail.com>
2022-04-26 20:36:50 +0200

    Better informational output in RunTests.ps1

--- RunTests.ps1
+++ RunTests.ps1
@@ -1,14 +1,17 @@
 $ProgressPreference = "silentlycontinue"
 
-Write-Host "Installing NuGet and Pester" -ForegroundColor Green
+Write-Host "Installing NuGet" -ForegroundColor Green
 Install-PackageProvider -Name NuGet -Force -Confirm:$false
 if(!(Get-InstalledModule Pester -ErrorAction "silentlyContinue")){
+    Write-Host "Installing Pester" -ForegroundColor Green
     Install-Module -Name Pester -MinimumVersion "5.0.0" -Force -SkipPublisherCheck -Confirm:$false
 }
 if(!(Get-InstalledModule PsIni -ErrorAction "silentlyContinue")){
+    Write-Host "Installing PsIni" -ForegroundColor Green
     Install-Module -Name PsIni -Force -Confirm:$false
 }
 if(!(Get-InstalledModule Recycle -ErrorAction "silentlyContinue")){
+    Write-Host "Installing Recycle" -ForegroundColor Green
     Install-Module -Name Recycle -Force -Confirm:$false
 }
 
