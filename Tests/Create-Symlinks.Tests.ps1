b924655002b4ed3367051f6e1c2ca505f7dbc088
Kevin <kevinholtkamp26@gmail.com>
2022-06-04 13:49:52 +0200

    Improve symlink tests

--- Tests/Create-Symlinks.Tests.ps1
+++ Tests/Create-Symlinks.Tests.ps1
@@ -6,40 +6,77 @@ BeforeAll {
 }
 
 Describe "Create-Symlinks"{
-    BeforeAll{
-        New-Item "TestDrive:\Original\Existing\Existing" -ItemType "directory" -Force
-        New-Item "TestDrive:\Original\Existing\New" -ItemType "directory" -Force
-        New-Item "TestDrive:\Links\Existing\Existing" -ItemType "directory" -Force
-        New-Item "TestDrive:\Links\New\Existing" -ItemType "directory" -Force
-        Set-Content "TestDrive:\Links\New\Existing\File.txt" "TestFileContentLinks" -Force
-        Set-Content "TestDrive:\Links\Existing\Existing\File.txt" "TestFileContentLinks" -Force
-        Set-Content "TestDrive:\Original\Existing\Existing\File.txt" "TestFileContentOriginal" -Force
-        Set-Content "TestDrive:\Original\Existing\New\File.txt" "TestFileContentOriginal" -Force
+    Context "Locked files"{
+        BeforeAll{
+            New-Item "TestDrive:\Original\Lock" -ItemType "directory" -Force
+            New-Item "TestDrive:\Original\Lock\File.txt" -ItemType "file" -Force
+            New-Item "TestDrive:\Target\Lock\File.txt" -ItemType "file" -Force
+        }
+        It "Locked file in target folder"{
+            $FileLock = [System.IO.File]::Open("$((Get-PSDrive "TestDrive").Root)\Target\Lock\File.txt", "Open", "Read")
 
-        Create-Symlinks -Group $TestGroup
+            {Create-Symlinks -Group $TestGroup -FileName "symlinks_lock.ini" -ErrorAction "stop"} | Should -Not -Throw
+
+            $FileLock.close()
+        }
+        It "Locked file in original folder"{
+            $FileLock = [System.IO.File]::Open("$((Get-PSDrive "TestDrive").Root)\Original\Lock\File.txt", "Open", "Read")
+
+            {Create-Symlinks -Group $TestGroup -FileName "symlinks_lock.ini" -ErrorAction "stop"} | Should -Throw
+
+            $FileLock.close()
+        }
     }
-    Context "Tests"{
+    Context "Normal cases"{
+        BeforeAll{
+            New-Item "TestDrive:\Original\Existing\Existing" -ItemType "directory" -Force
+            New-Item "TestDrive:\Original\Existing\New" -ItemType "directory" -Force
+            New-Item "TestDrive:\Links\Existing\Existing" -ItemType "directory" -Force
+            New-Item "TestDrive:\Links\New\Existing" -ItemType "directory" -Force
+            Set-Content "TestDrive:\Original\Existing\Existing\File.txt" "TestFileContentOriginal" -Force
+            Set-Content "TestDrive:\Original\Existing\New\File.txt" "TestFileContentOriginal" -Force
+            Set-Content "TestDrive:\Links\New\Existing\File.txt" "TestFileContentLinks" -Force
+            Set-Content "TestDrive:\Links\Existing\Existing\File.txt" "TestFileContentLinks" -Force
+
+            Create-Symlinks -Group $TestGroup -FileName "symlinks.ini" -ErrorAction "silentlycontinue"
+        }
         It "Nothing Exists"{
             Test-Symlink "TestDrive:\Original\New\New" | Should -Be $true
             "TestDrive:\Links\New\New" | Should -Exist
         }
-        It "Existing Original folder"{
+        It "Original folder exists"{
             Test-Symlink "TestDrive:\Original\Existing\New" | Should -Be $true
             "TestDrive:\Links\Existing\New" | Should -Exist
             "TestDrive:\Links\Existing\New\File.txt" | Should -FileContentMatch "TestFileContentOriginal"
             "TestDrive:\Original\Existing\New\File.txt" | Should -FileContentMatch "TestFileContentOriginal"
         }
-        It "Existing LinkPath"{
+        It "LinkPath exists"{
             Test-Symlink "TestDrive:\Original\New\Existing" | Should -Be $true
             "TestDrive:\Links\New\Existing" | Should -Exist
             "TestDrive:\Links\New\Existing\File.txt" | Should -FileContentMatch "TestFileContentLinks"
             "TestDrive:\Original\New\Existing\File.txt" | Should -FileContentMatch "TestFileContentLinks"
         }
-        It "Both existing"{
+        It "Both exist"{
             Test-Symlink "TestDrive:\Original\Existing\Existing" | Should -Be $true
             "TestDrive:\Links\Existing\Existing" | Should -Exist
             "TestDrive:\Links\Existing\Existing\File.txt" | Should -FileContentMatch "TestFileContentOriginal"
             "TestDrive:\Original\Existing\Existing\File.txt" | Should -FileContentMatch "TestFileContentOriginal"
+        }
+    }
+    Context "Folder exist as file"{
+        It "LinkPath is file"{
+            New-Item "TestDrive:\Original\FileTest" -ItemType "directory" -Force
+            New-Item "TestDrive:\Original\FileTest\File" -ItemType "file" -Force
+            New-Item "TestDrive:\Links\FileTest" -ItemType "file" -Force
+
+            {Create-Symlinks -Group $TestGroup -FileName "symlinks_file.ini" -ErrorAction "stop"} | Should -Throw
+        }
+        It "Original path is file"{
+            New-Item "TestDrive:\Original\FileTest" -ItemType "file" -Force
+            New-Item "TestDrive:\Links\FileTest" -ItemType "directory" -Force
+
+            {Create-Symlinks -Group $TestGroup -FileName "symlinks_file.ini" -ErrorAction "stop"} | Should -Throw
+            Test-Symlink "TestDrive:\Original\FileTest" | Should -Be $false
         }
     }
 }
