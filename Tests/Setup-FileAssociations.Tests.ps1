7327800a04266257fc1ed112ceae7764d1c4f45d
Kevin <kevinholtkamp26@gmail.com>
2022-06-06 23:19:16 +0200

    Added testing for Setup-FileAssociations

--- Tests/Setup-FileAssociations.Tests.ps1
+++ Tests/Setup-FileAssociations.Tests.ps1
@@ -1,14 +1,23 @@
 BeforeAll {
     . .\Tests\CommonTestParameters.ps1
 
-    . .\setup.ps1
+    . .\functions.ps1
 }
 
+#Testing the helper function Create-Association since Setup-FileAssociations is just a wrapper and ini reader
 Describe "Setup-FileAssociations"{
-    #    Context "Setup-FileAssociations"{
-    #        BeforeAll{
-    #            Setup-FileAssociations -Group $TestGroup
-    #        }
-    #        #ToDo Test for Setup-FileAssociations
-    #    }
+    Context "Setup-FileAssociations"{
+        BeforeAll{
+            $AssociationBefore = cmd.exe /c "assoc .csv"
+            $AssociationBefore = $AssociationBefore.Split('=')[1]
+        }
+        AfterAll{
+            Create-Association ".csv" $AssociationBefore
+        }
+        It "Setting Editor for .csv"{
+#            Setup-FileAssociations -Group $TestGroup
+            Create-Association ".csv" "C:\Windows\system32\notepad.exe"
+            cmd.exe /c "assoc .csv" | Should -Be ".csv=C:\Windows\system32\notepad.exe"
+        }
+    }
 }
