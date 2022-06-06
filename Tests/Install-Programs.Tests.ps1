e80fc19a21e164565aeab11540a775e66339e636
Kevin <kevinholtkamp26@gmail.com>
2022-06-06 23:22:16 +0200

    Added test for Install-Programs, some of which are yet to be implemented

--- Tests/Install-Programs.Tests.ps1
+++ Tests/Install-Programs.Tests.ps1
@@ -6,25 +6,29 @@ BeforeAll {
 }
 
 Describe "Install-Programs"{
-    Context "Install-Programs"{
+    Context "Install from URL"{
         BeforeAll{
             Mock Start-Process {}
-            Mock Start-Job {}
-            function script:choco(){}
-            function script:winget(){}
-#            Mock choco {}
-#            Mock winget {}
         }
         It "Tests"{
             Install-Programs -Group $TestGroup
-            #Install Notepad
             Should -Invoke Start-Process -Times 1 -Exactly -ParameterFilter {$FilePath -eq "$($Env:TEMP)\1.exe"}
             "$($Env:TEMP)\1.exe" | Should -Not -Exist
-            #ToDo add proper mocking for choco and winget
-            #Global confirmation, 2x for source, 4x for install / pin
-#            Should -Invoke -CommandName choco -Times 7 -Exactly -ParameterFiler {}
-            #Winget install
-#            Should -Invoke -CommandName winget -Times 2 -Exactly
+        }
+    }
+    Context "Install from exe"{
+        It "Not implemented"{
+            $implemented | Should -Be $true
+        }
+    }
+    Context "Install from choco"{
+        It "Not implemented"{
+            $implemented | Should -Be $true
+        }
+    }
+    Context "Install from winget"{
+        It "Not implemented"{
+            $implemented | Should -Be $true
         }
     }
 }
