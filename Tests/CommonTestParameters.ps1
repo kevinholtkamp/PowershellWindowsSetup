7188e38f050e5886a5080c31b5f8ecf34b1ae997
Kevin <kevinholtkamp26@gmail.com>
2022-06-15 22:42:38 +0200

    Changed variables in CommonTestParameters.ps1 to Script scope for non-intrusive

--- Tests/CommonTestParameters.ps1
+++ Tests/CommonTestParameters.ps1
@@ -1,10 +1,10 @@
-Set-Variable -Name "TestGroup" -Value "Tests\testGroup" -Scope Global
+Set-Variable -Name "TestGroup" -Value "Tests\testGroup" -Scope Script
 
 Mock -CommandName Write-Host -MockWith {}
 Mock -CommandName Write-Debug -MockWith {}
 Mock -CommandName Write-Verbose -MockWith {}
 Mock -CommandName Read-Host -MockWith {}
-Set-Variable -Name "DebugPreference" -Value "silentlyContinue" -Scope Global
-Set-Variable -Name "ErrorActionPreference" -Value "silentlyContinue" -Scope Global
-Set-Variable -Name "VerbosePreference" -Value "silentlyContinue" -Scope Global
-Set-Variable -Name "ProgressPreference" -Value "silentlyContinue" -Scope Global
+Set-Variable -Name "DebugPreference" -Value "silentlyContinue" -Scope Script
+Set-Variable -Name "ErrorActionPreference" -Value "silentlyContinue" -Scope Script
+Set-Variable -Name "VerbosePreference" -Value "silentlyContinue" -Scope Script
+Set-Variable -Name "ProgressPreference" -Value "silentlyContinue" -Scope Script
