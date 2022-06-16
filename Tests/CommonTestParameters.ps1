60ee7f577372c052d254480b9d9a9d33bf227f47
Kevin <kevinholtkamp26@gmail.com>
2022-06-16 18:03:19 +0200

    Removed preference variables from CommonTestParameters.ps1 since they will be set by RunTests.ps1

--- Tests/CommonTestParameters.ps1
+++ Tests/CommonTestParameters.ps1
@@ -4,10 +4,6 @@ Mock -CommandName Write-Host -MockWith {}
 Mock -CommandName Write-Debug -MockWith {}
 Mock -CommandName Write-Verbose -MockWith {}
 Mock -CommandName Read-Host -MockWith {Write-Host $Prompt}
-Set-Variable -Name "DebugPreference" -Value "silentlyContinue" -Scope Script
-Set-Variable -Name "ErrorActionPreference" -Value "silentlyContinue" -Scope Script
-Set-Variable -Name "VerbosePreference" -Value "silentlyContinue" -Scope Script
-Set-Variable -Name "ProgressPreference" -Value "silentlyContinue" -Scope Script
 
 . .\functions.ps1
 . .\setup.ps1
