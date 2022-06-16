f8fc22f77e7bdf2f56307f49530b5e85cf24d4ac
Kevin <kevinholtkamp26@gmail.com>
2022-06-16 11:52:01 +0200

    Made Setup-Hosts.Tests.ps1 create its own configuration files and clean them up afterwards

--- Tests/Setup-Hosts.Tests.ps1
+++ Tests/Setup-Hosts.Tests.ps1
@@ -9,14 +9,25 @@ Describe "Setup-Hosts"{
     Context "Setup-Hosts"{
         BeforeAll{
             $Env:WinDir = "TestDrive:\Windows"
-            New-Item "$($Env:WinDir)\system32\Drivers\etc\hosts" -ItemType File -Force
-            Setup-Hosts -Group $TestGroup
+            New-Item "$($Env:WinDir)\system32\Drivers\etc\hosts" -ItemType File -Force -ErrorAction SilentlyContinue
+        }
+        AfterAll{
             $Env:WinDir = "C:\Windows"
+            Remove-Item "$TestGroup\hosts\from-file.txt"
+            Remove-Item "$TestGroup\hosts\from-url.txt"
         }
         It "Importing hosts from file"{
+            Set-Content "$TestGroup\hosts\from-file.txt" "192.168.178.1       fritz.box"
+
+            Setup-Hosts -Group $TestGroup
+
             Select-String -Path "TestDrive:\Windows\system32\Drivers\etc\hosts" -Pattern "fritz.box" | Should -not -BeNullOrEmpty
         }
         It "Importing hosts from url"{
+            Set-Content "$TestGroup\hosts\from-url.txt" "https://blocklistproject.github.io/Lists/smart-tv.txt"
+
+            Setup-Hosts -Group $TestGroup
+
             Select-String -Path "TestDrive:\Windows\system32\Drivers\etc\hosts" -Pattern "Smart TV list" | Should -not -BeNullOrEmpty
         }
     }
