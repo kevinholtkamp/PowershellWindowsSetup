d845fa98feb9e22643e37ef39e9c85ee245e4531
Kevin <kevinholtkamp26@gmail.com>
2022-06-16 15:20:52 +0200

    Added automated testing using the windows sandbox

---
+++ RunTestSandbox.ps1
@@ -0,0 +1,16 @@
+$ConfigFile = "$(Get-Location)/Tests/TestSandbox.wsb"
+$SandboxExecutable = "C:\Windows\system32\WindowsSandbox.exe"
+
+if(Test-Path $SandboxExecutable){
+    #Add Host folder to sandbox config file
+    (Get-Content -Path $ConfigFile -Raw) -replace "<HostFolder>.*</HostFolder>", "<HostFolder>$(Get-Location)</HostFolder>" `
+    | Out-File $ConfigFile
+
+    #Launch Sandbox with configuration file
+    Start-Process -WindowStyle ([System.Diagnostics.ProcessWindowStyle]::Maximized) `
+    -FilePath $SandboxExecutable `
+    -ArgumentList $ConfigFile
+}
+else{
+    Write-Error "Sandbox not installed"
+}
