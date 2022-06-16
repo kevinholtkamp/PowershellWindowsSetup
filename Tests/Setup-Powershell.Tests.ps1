ced6f65dbe2020cbceeb1c29f2422f91029ea1d9
Kevin <kevinholtkamp26@gmail.com>
2022-06-16 11:52:50 +0200

    Made Setup-Powershell.Tests.ps1 create its own configuration files and clean them up afterwards

--- Tests/Setup-Powershell.Tests.ps1
+++ Tests/Setup-Powershell.Tests.ps1
@@ -13,12 +13,20 @@ Describe "Setup-Powershell"{
             Mock Get-PackageProvider {}
             Mock Install-PackageProvider {}
             Mock Install-Module {}
+
+            Set-Content "$TestGroup\powershell\module.txt" "PSWinRAR"
+            Set-Content "$TestGroup\powershell\packageprovider.txt" "NuGet"
+        }
+        AfterAll{
+            Remove-Item "$TestGroup\powershell\module.txt"
+            Remove-Item "$TestGroup\powershell\packageprovider.txt"
         }
         It "Testing install"{
             Setup-Powershell -Group $TestGroup
+
             Should -Invoke Update-Help -Exactly -Times 1
-            Should -Invoke Install-PackageProvider -Exactly -Times 1 -ParameterFilter {"NuGet"}
             Should -Invoke Get-PackageProvider -Exactly -Times 1 -ParameterFilter {"NuGet"}
+            Should -Invoke Install-PackageProvider -Exactly -Times 1 -ParameterFilter {"NuGet"}
             Should -Invoke Get-InstalledModule -Exactly -Times 1 -ParameterFilter {"PSWinRAR"}
             Should -Invoke Install-Module -Exactly -Times 1 -ParameterFilter {"PSWinRAR"}
         }
