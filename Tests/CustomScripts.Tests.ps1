d6096d730cd2340e312eee2fe2eb1b127e35f592
Kevin <kevinholtkamp26@gmail.com>
2022-06-16 11:46:11 +0200

    Made CustomScripts.Tests.ps1 create its own configuration files and clean them up afterwards

--- Tests/CustomScripts.Tests.ps1
+++ Tests/CustomScripts.Tests.ps1
@@ -3,12 +3,20 @@ BeforeAll {
 }
 
 Describe "Custom scripts"{
+    AfterAll{
+        Remove-Item "$TestGroup\scripts\append_custom.ps1"
+        Remove-Item "$TestGroup\scripts\prepend_custom.ps1"
+    }
     It "Prepend-Script"{
+        Set-Content "$TestGroup\scripts\prepend_custom.ps1" 'Set-Variable -Name "ExecutedPrepend" -Value $true -Scope Global'
+
         . "$TestGroup\scripts\prepend_custom.ps1"
 
         $ExecutedPrepend | Should -Be $true
     }
     It "Append-Script"{
+        Set-Content "$TestGroup\scripts\append_custom.ps1" 'Set-Variable -Name "ExecutedAppend" -Value $true -Scope Global'
+
         . "$TestGroup\scripts\append_custom.ps1"
 
         $ExecutedAppend | Should -Be $true
