5058dff20f09b57426b360257579f0cdc2cb850a
Kevin <kevinholtkamp26@gmail.com>
2022-06-16 17:27:30 +0200

    Auto trim TestSandbox.wsb

--- RunTestSandbox.ps1
+++ RunTestSandbox.ps1
@@ -3,7 +3,7 @@ $SandboxExecutable = "C:\Windows\system32\WindowsSandbox.exe"
 
 if(Test-Path $SandboxExecutable){
     #Add Host folder to sandbox config file
-    (Get-Content -Path $ConfigFile -Raw) -replace "<HostFolder>.*</HostFolder>", "<HostFolder>$(Get-Location)</HostFolder>" `
+    ((Get-Content -Path $ConfigFile -Raw) -replace "<HostFolder>.*</HostFolder>", "<HostFolder>$(Get-Location)</HostFolder>").Trim() `
     | Out-File $ConfigFile
 
     #Launch Sandbox with configuration file
