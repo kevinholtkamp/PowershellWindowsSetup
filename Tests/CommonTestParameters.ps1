67026d60f7e96c1dcfb556835e473d2a29a3fa72
Kevin <kevinholtkamp26@gmail.com>
2022-06-16 15:24:24 +0200

    Made mocked Read-Host invokes Write-Host

--- Tests/CommonTestParameters.ps1
+++ Tests/CommonTestParameters.ps1
@@ -3,7 +3,7 @@ Set-Variable -Name "TestGroup" -Value "Tests\testGroup" -Scope Script
 Mock -CommandName Write-Host -MockWith {}
 Mock -CommandName Write-Debug -MockWith {}
 Mock -CommandName Write-Verbose -MockWith {}
-Mock -CommandName Read-Host -MockWith {}
+Mock -CommandName Read-Host -MockWith {Write-Host $Prompt}
 Set-Variable -Name "DebugPreference" -Value "silentlyContinue" -Scope Script
 Set-Variable -Name "ErrorActionPreference" -Value "silentlyContinue" -Scope Script
 Set-Variable -Name "VerbosePreference" -Value "silentlyContinue" -Scope Script
