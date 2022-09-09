1861c6cfefdcd84f42e3e462c6205e0bdead2223
Kevin <kevinholtkamp26@gmail.com>
2022-09-09 22:10:49 +0200

    Merged imp_Better_Parameters

--- Tests/Setup-FileAssociations.Tests.ps1
+++ Tests/Setup-FileAssociations.Tests.ps1
@@ -5,31 +5,36 @@ BeforeAll {
 Describe "Setup-FileAssociations"{
     BeforeAll{
         New-Item "$TestConfiguration\settings" -ItemType Directory -Force -ErrorAction SilentlyContinue
+
+        $AssociationBefore = Get-FTA ".abc"
     }
     AfterAll{
         Remove-Item "$TestConfiguration\settings" -Force -Recurse -ErrorAction SilentlyContinue
-    }
-    Context "Setup-FileAssociations"{
-        BeforeAll{
-            $AssociationBefore = Get-FTA ".abc"
 
+        if($AssociationBefore){
+            Register-FTA $AssociationBefore ".acb"
+        }
+        else{
+            Remove-FTA "C:\Windows\system32\notepad.exe" ".abc"
+        }
+
+        Get-FTA ".abc" | Should -Be $AssociationBefore
+
+        Remove-Item "$TestConfiguration\settings\associations.ini" -Force -ErrorAction "SilentlyContinue"
+    }
+    Context "Configuration parameter"{
+        It "Setting Editor for .csv"{
             Set-Content "$TestConfiguration\settings\associations.ini" '[associations]'
             Add-Content "$TestConfiguration\settings\associations.ini" '.abc="C:\Windows\system32\notepad.exe"'
-        }
-        AfterAll{
-            if($AssociationBefore){
-                Register-FTA $AssociationBefore ".acb"
-            }
-            else{
-                Remove-FTA "C:\Windows\system32\notepad.exe" ".abc"
-            }
 
-            Get-FTA ".abc" | Should -Be $AssociationBefore
+            Setup-FileAssociations -Configuration $TestConfiguration
 
-            Remove-Item "$TestConfiguration\settings\associations.ini"
+            Get-FTA ".abc" | Should -Be "SFTA.notepad.abc"
         }
+    }
+    Context "IniContent parameter"{
         It "Setting Editor for .csv"{
-            Setup-FileAssociations -Configuration $TestConfiguration -Verbose
+            Setup-FileAssociations -IniContent @{associations = @{".abc" = "C:\Windows\system32\notepad.exe"}}
 
             Get-FTA ".abc" | Should -Be "SFTA.notepad.abc"
         }
