e6b745c73cc6a699fcb7942fd1875c02a0f6f802
Kevin <kevinholtkamp26@gmail.com>
2023-05-14 18:09:50 +0200

    Improved formatting and uncomment mock for testing

--- Tests/CommonTestParameters.ps1
+++ Tests/CommonTestParameters.ps1
@@ -1,9 +1,9 @@
 Set-Variable -Name "TestConfiguration" -Value "Tests\testConfiguration" -Scope Script
 
-#Mock -CommandName Write-Host -MockWith {}
-#Mock -CommandName Write-Debug -MockWith {}
-#Mock -CommandName Write-Verbose -MockWith {}
-#Mock -CommandName Read-Host -MockWith {Write-Host $Prompt}
-#Mock -CommandName Write-Information -MockWith {}
+Mock -CommandName Write-Host -MockWith {}
+Mock -CommandName Write-Debug -MockWith {}
+Mock -CommandName Write-Verbose -MockWith {}
+Mock -CommandName Read-Host -MockWith {Write-Host $Prompt}
+Mock -CommandName Write-Information -MockWith {}
 
 . .\Run-Setup.ps1
