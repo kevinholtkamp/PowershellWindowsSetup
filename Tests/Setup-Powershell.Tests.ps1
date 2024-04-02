a019ee2841ce8f3e7d7a9c19c61f4aa039e837c3
Kevin <kevinholtkamp26@gmail.com>
2024-04-02 02:32:02 +0200

    Setup-Powershell

--- Tests/Setup-Powershell.Tests.ps1
+++ Tests/Setup-Powershell.Tests.ps1
@@ -9,31 +9,6 @@ Describe "Setup-Powershell"{
     AfterAll{
         Remove-Item "$TestConfiguration\powershell" -Force -Recurse -ErrorAction SilentlyContinue
     }
-    Context "Configuration parameter"{
-        BeforeAll{
-            Mock Update-Help {}
-            Mock Get-InstalledModule {}
-            Mock Get-PackageProvider {}
-            Mock Install-PackageProvider {}
-            Mock Install-Module {}
-
-            Set-Content "$TestConfiguration\powershell\module.txt" "PSWinRAR"
-            Set-Content "$TestConfiguration\powershell\packageprovider.txt" "NuGet"
-        }
-        AfterAll{
-            Remove-Item "$TestConfiguration\powershell\module.txt"
-            Remove-Item "$TestConfiguration\powershell\packageprovider.txt"
-        }
-        It "Testing install"{
-            Setup-Powershell -Configuration $TestConfiguration
-
-            Should -Invoke Update-Help -Exactly -Times 1
-            Should -Invoke Get-PackageProvider -Exactly -Times 1 -ParameterFilter {"NuGet"}
-            Should -Invoke Install-PackageProvider -Exactly -Times 1 -ParameterFilter {"NuGet"}
-            Should -Invoke Get-InstalledModule -Exactly -Times 1 -ParameterFilter {"PSWinRAR"}
-            Should -Invoke Install-Module -Exactly -Times 1 -ParameterFilter {"PSWinRAR"}
-        }
-    }
     Context "PackageProvider parameter"{
         BeforeAll{
             Mock Update-Help {}
@@ -43,13 +18,11 @@ Describe "Setup-Powershell"{
             Mock Install-Module {}
         }
         It "Testing install"{
-            Setup-Powershell -Configuration "" -Modules "PSWinRAR" -PackageProvider "NuGet"
+            Setup-Powershell -PackageProvider @("NuGet")
 
             Should -Invoke Update-Help -Exactly -Times 1
             Should -Invoke Get-PackageProvider -Exactly -Times 1 -ParameterFilter {"NuGet"}
             Should -Invoke Install-PackageProvider -Exactly -Times 1 -ParameterFilter {"NuGet"}
-            Should -Invoke Get-InstalledModule -Exactly -Times 1 -ParameterFilter {"PSWinRAR"}
-            Should -Invoke Install-Module -Exactly -Times 1 -ParameterFilter {"PSWinRAR"}
         }
     }
     Context "Modules parameter"{
@@ -61,7 +34,7 @@ Describe "Setup-Powershell"{
             Mock Install-Module {}
         }
         It "Testing install"{
-            Setup-Powershell -Configuration "" -Modules @("PSWinRAR", "NuGet")
+            Setup-Powershell -Modules @("PSWinRAR", "NuGet")
 
             Should -Invoke Update-Help -Exactly -Times 1
             Should -Invoke Get-InstalledModule -Exactly -Times 2 -ParameterFilter {"PSWinRAR"}
