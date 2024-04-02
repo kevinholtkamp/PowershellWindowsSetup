94eda201f26d16c1868dc348a745cd6d49912104
Kevin <kevinholtkamp26@gmail.com>
2024-04-02 02:36:26 +0200

    Mock Write-Verbose

--- Tests/CommonTestParameters.ps1
+++ Tests/CommonTestParameters.ps1
@@ -2,7 +2,7 @@ Set-Variable -Name "TestConfiguration" -Value "Tests\testConfiguration" -Scope S
 
 Mock -CommandName Write-Host -MockWith {}
 Mock -CommandName Write-Debug -MockWith {}
-#Mock -CommandName Write-Verbose -MockWith {}
+Mock -CommandName Write-Verbose -MockWith {}
 Mock -CommandName Read-Host -MockWith {Write-Host $Prompt}
 Mock -CommandName Write-Information -MockWith {}
 
