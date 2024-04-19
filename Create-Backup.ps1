f4367da7580fbe35ccceb2ff88b5c3b960d2e57e
Kevin <kevinholtkamp26@gmail.com>
2024-04-19 12:27:58 +0200

    .

--- Create-Backup.ps1
+++ Create-Backup.ps1
@@ -1,60 +1,41 @@
 param(
     [Parameter(Position = 0)]
-    [String] $Configuration = "default",
-
-    [Parameter(Position = 1)]
-    [ValidateSet("WinRAR", "Zip")]
-    [String] $Compression = "WinRAR"
+    [String] $Configuration = "default"
 )
 
-"PSHelperTools" | ForEach-Object {
-    if(!(Get-InstalledModule $_ -ErrorAction SilentlyContinue)){
-        Install-Module $_ -Force -ErrorAction Stop
-        Import-Module $_ -Force -ErrorAction Stop
-    }
-}
 
-if(PromptYesNo "Do you want to manually enter files to backup?"){
-    $Files = Read-ArrayInput "Files, directories or wildcards"
-}
-else{
-    if(PromptYesNo "Do you want to use recommended settings instead of a configuration?"){
-        $Files = @(
-            "C:\Users\$($Env:UserName)\AppData\Roaming",
-            "C:\Users\$($Env:UserName)\Desktop",
-            "C:\Users\$($Env:UserName)\Documents",
-            "C:\Users\$($Env:UserName)\Music",
-            "C:\Users\$($Env:UserName)\Pictures",
-            "C:\Users\$($Env:UserName)\Videos",
-            "C:\Program Files (x86)",
-            "C:\Program Files",
-            "C:\ProgramData"
-        )
-    }
-    else{
-        Write-Host "Enter the configuration name: " -ForegroundColor Green -NoNewline
-        $Configuration = Read-Host
-        $Files = Get-Content "./$Configuration/backup.txt" | Where-Object {$_ -notlike ";*"}
-    }
-}
+$config = Get-IniContent ".\$Configuration\backup\backup.ini"
 
-if($Compression -eq "WinRAR"){
-    if(!(Get-InstalledModule "PSWinRAR" -SilentlyContinue)){
-        Install-Module "PSWinRAR" -Force -ErrorAction Stop
-        Import-Module "PSWinRAR" -Force -ErrorAction Stop
-    }
-    $Files | Compress-WinRAR `
+$Compression
+$Files = Get-Content ".\$Configuration\backup\backup_files.txt" | Where-Object {$_ -notlike ";*"}
+
+if($config["winget"] -eq "true"){
+    winget list | Add-Content ".\$Configuration\install\from-winget.txt"
+}
+if($config["choco"] -eq "true"){
+    choco list --localonly | Add-Content ".\$Configuration\install\from-winget.txt"
+}
+switch ($Compression.ToLowercase()){
+    "winrar" {
+        if(!(Get-InstalledModule "PSWinRAR" -SilentlyContinue)){
+            Install-Module "PSWinRAR" -Force -ErrorAction Stop
+            Import-Module "PSWinRAR" -Force -ErrorAction Stop
+        }
+        $Files | Compress-WinRAR `
         -Archive "$(Get-Location)\Backup.rar" `
         -ArchiveFileStructure "Full" `
         -Preset "High" `
         -ErrorAction "Continue" `
         -RecoveryPercentage 5 `
         -Verbose
-}
-else{
-    Compress-Archive `
+    }
+    "zip" {
+        Compress-Archive `
         -Path $Files `
         -DestinationPath "$(Get-Location)\$Configuration\Backup.zip" `
         -CompressionLevel Optimal `
         -Verbose
+    }
+    Default{
+    }
 }
