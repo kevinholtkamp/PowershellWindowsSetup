8d3390b9fa89fd15bf529ef400475d58d7559d52
Kevin Holtkamp <kevinholtkamp26@gmail.com>
2024-05-16 12:18:56 +0200

    Syntax fix

--- Create-Backup.ps1
+++ Create-Backup.ps1
@@ -8,18 +8,19 @@ $config = Get-IniContent ".\$Configuration\backup\backup.ini"
 
 $Files = Get-Content ".\$Configuration\backup\backup_directories.txt" | Where-Object {$_ -notlike ";*"}
 
-if($config["settings"]["winget"] -eq "true"){
-    winget list | Add-Content ".\$Configuration\install\from-winget.txt"
-}
-if($config["settings"]["choco"] -eq "true"){
-    choco list --localonly | Add-Content ".\$Configuration\install\from-winget.txt"
-}
-switch ($config["settings"]["archiver"].ToLowercase()){
+
+#if($config["settings"]["winget"] -eq "true"){
+#    winget list | Add-Content ".\$Configuration\install\from-winget.txt"
+#}
+#if($config["settings"]["choco"] -eq "true"){
+#    choco list --localonly | Add-Content ".\$Configuration\install\from-winget.txt"
+#}
+switch ($config["settings"]["archiver"].ToLower()){
     "winrar" {
-        if(!(Get-InstalledModule "PSWinRAR" -SilentlyContinue)){
-            Install-Module "PSWinRAR" -Force -ErrorAction Stop
-            Import-Module "PSWinRAR" -Force -ErrorAction Stop
-        }
+#        if(!(Get-InstalledModule "PSWinRAR" -ErrorAction SilentlyContinue)){
+#            Install-Module "PSWinRAR" -Force -ErrorAction Stop
+#            Import-Module "PSWinRAR" -Force -ErrorAction Stop
+#        }
         $Files | Compress-WinRAR `
         -Archive "$(Get-Location)\$Configuration\Backup.rar" `
         -ArchiveFileStructure "Full" `
